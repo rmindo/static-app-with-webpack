515e41aae12121fa8e476e61c59433d628325d23
rmindo <wrelmind@gmail.com>
2024-05-09 07:52:49 +0800

    Create entry file and initiate the app

---
+++ index.js
@@ -0,0 +1,13 @@
+import './public/static/css/fonts.scss';
+import './public/static/css/global.scss';
+import './public/static/css/layout.scss';
+import './public/static/css/main.scss';
+import './public/static/css/mobile.scss';
+
+
+import app from './src/app'
+
+/**
+ * Initiate
+ */
+app.init()
